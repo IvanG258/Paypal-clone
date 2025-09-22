92e60b17106a0d383fd67cfdcacc002cca245f24
Ivan Guitimela <135559066+IvanG258@users.noreply.github.com>
2025-09-22 20:51:04 +1200

    adicao de idiomas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
+
 import { Geist, Geist_Mono } from "next/font/google";
-import Login from './components/Login.tsx'
 import {useState} from "react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,20 +17,32 @@ export default function Home() {
   return (
      <div className=" ">
    
-      <main className="border border-gray-500 p-2 rounded-[10] ml-auto mr-auto mt-[70]   h-[600px] w-[450px] flex flex-col row-start-2 text-center items-center ">
+      <main className="border border-gray-500 p-2 rounded-[10] ml-auto mr-auto mt-[100]   h-[600] w-[450] flex flex-col row-start-2 text-center items-center ">
 
      <h1 className="pt-[30px] font-bold">GPay</h1>
      <input 
      type="text" 
-     placeholder="Email"
+     placeholder="Email or username"
      value={value}
      onChange= {(e)=>setValue(e.target.value)}
-     className="border p-2 rounded w-[350px] mt-[60]"
+     className="border p-2 rounded w-[350] h-[60] mt-[60]"
      />
      <a href="#" className="text-[#1161ca] text-start font-semibold">Forget Email?</a>
      <button className="bg-[#1161ca] text-white font-semibold  h-[50] mt-[40] cursor-pointer p-2 rounded-[30px] w-[350px]">Next</button>
-      <hr className="my-4 border-red-800" />  <hr className="my-4 border-red-800" />
+        <div className="mt-[20] mb-[20] flex items-center">
+        <hr className="flex-1 w-[150] border-gray-400" />
+        <span className="mx-4 text-gray-600">or</span>
+        <hr className="flex-1 w-[150] border-gray-400" />
+      </div>
       <button className="border-[3px] cursor-pointer h-[50] p-2 rounded-[30px] w-[350px] hover:border-[#1161ca] hover:text-[#1161ca]">Sign Up</button>
+    
+    <ul className="flex mt-[120] justify-center align-center text-center ">
+      <li className="p-[4]"><select><option>US</option></select></li>
+      <li className="p-[4] border-left-gray-400">English</li>
+     <li className="p-[4] border-left-gray-400"> Spanish</li>
+      <li className="p-[4] border-left-gray-400">Francais</li>
+      <li className="p-[4] border-left-gray-400">Deutsch</li>
+      </ul>
     
      </main>
     <ul className="flex mt-[40] justify-center align-center text-center ">
@@ -41,11 +52,7 @@ export default function Home() {
       <li className="p-[4]">PoliceUpdate</li>
       <li className="p-[4]">Worldwide</li>
       </ul>
-      <div className="flex items-center">
-        <hr className="flex-1 border-gray-400" />
-        <span className="mx-4 text-gray-600">Minha Palavra</span>
-        <hr className="flex-1 border-gray-400" />
-      </div>
+    
     </div>
   );
 }
