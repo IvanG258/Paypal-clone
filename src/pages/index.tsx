05b91faba4fa2f6e5f77fb4a7731d2b30d510e50
Ivan Guitimela <135559066+IvanG258@users.noreply.github.com>
2025-09-22 19:13:17 +1200

    adicao de hr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,9 +16,9 @@ const geistMono = Geist_Mono({
 export default function Home() {
  const [value, setValue]=useState("");
   return (
-     <div className=" min-h-screen ">
+     <div className=" ">
    
-      <main className="border p-2 rounded-[10] ml-auto mr-auto mt-[70]   h-[600px] w-[450px] flex flex-col row-start-2 text-center items-center ">
+      <main className="border border-gray-500 p-2 rounded-[10] ml-auto mr-auto mt-[70]   h-[600px] w-[450px] flex flex-col row-start-2 text-center items-center ">
 
      <h1 className="pt-[30px] font-bold">GPay</h1>
      <input 
@@ -29,9 +29,10 @@ export default function Home() {
      className="border p-2 rounded w-[350px] mt-[60]"
      />
      <a href="#" className="text-[#1161ca] text-start font-semibold">Forget Email?</a>
-     <button className="bg-[#1161ca] text-white-600  mt-[40] cursor-pointer p-2 rounded-[30px] w-[350px]">Next</button>
-      <hr className="my-4 border-red-800" />
-      <button className="border-[3px] cursor-pointer p-2 rounded-[30px] w-[350px]">Sign Up</button>
+     <button className="bg-[#1161ca] text-white font-semibold  h-[50] mt-[40] cursor-pointer p-2 rounded-[30px] w-[350px]">Next</button>
+      <hr className="my-4 border-red-800" />  <hr className="my-4 border-red-800" />
+      <button className="border-[3px] cursor-pointer h-[50] p-2 rounded-[30px] w-[350px] hover:border-[#1161ca] hover:text-[#1161ca]">Sign Up</button>
+    
      </main>
     <ul className="flex mt-[40] justify-center align-center text-center ">
       <li className="p-[4]">Contact Us</li>
@@ -40,6 +41,11 @@ export default function Home() {
       <li className="p-[4]">PoliceUpdate</li>
       <li className="p-[4]">Worldwide</li>
       </ul>
+      <div className="flex items-center">
+        <hr className="flex-1 border-gray-400" />
+        <span className="mx-4 text-gray-600">Minha Palavra</span>
+        <hr className="flex-1 border-gray-400" />
+      </div>
     </div>
   );
 }
